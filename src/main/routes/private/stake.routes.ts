2ab6f689baf68f95e2c61102044b131524f65abc
iti <italo.felipe.ignacio@hotmail.com>
2024-05-22 19:25:43 -0300

    dsaas

--- src/main/routes/private/stake.routes.ts
+++ src/main/routes/private/stake.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { stakeFirstAccessController } from '../../../application/controller/stake';
+import {
+  stakeFirstAccessController,
+  stakeLoginCodeController
+} from '../../../application/controller/stake';
 
 export const StakeRoutes = (inputRouter: Router): void => {
   const router = Router();
 
   router.post('/first-access', stakeFirstAccessController());
+  router.post('/login-code', stakeLoginCodeController());
 
   inputRouter.use('/stake', router);
 };
