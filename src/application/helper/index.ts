b459da1626e060d7159b235df7fcc9187b88cba4
iti <italo.felipe.ignacio@hotmail.com>
2024-05-28 20:05:39 -0300

    fsdds

--- src/application/helper/index.ts
+++ src/application/helper/index.ts
@@ -1,6 +1,7 @@
 export * from './has-user-by-username';
 export * from './has-user-by-id';
 export * from './find-email';
+export * from './create-action';
 export * from './get-google-sheets';
 export * from './read-sheet';
 export * from './convert-result';
