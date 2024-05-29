1d5a7ddcda6e571aee951ffd3d2c3371c42a6c2a
italo <italo.ignacio@sp.senai.br>
2024-05-29 14:33:09 -0300

    fix

--- src/application/controller/stake/login-code/index.ts
+++ src/application/controller/stake/login-code/index.ts
@@ -1,33 +1,29 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable consistent-return */
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-/* eslint-disable max-nested-callbacks */
 import { DataSource } from '../../../../infra/database';
-import { ValidationError } from 'yup';
+import { badRequest, errorLogger, messageErrorResponse, ok } from '../../../../main/utils';
+import { findEmail } from '../../../helper/find-email';
 import {
-  errorLogger,
-  messageErrorResponse,
-  notFound,
-  ok,
-  validationErrorResponse
-} from '../../../../main/utils';
-import { insertEmailSchema } from '../../../../data/validation';
-import Imap from 'imap';
+  type functionToExecProps,
+  readGoogleSheet,
+  type readGoogleSheetProps
+} from '../../../helper/read-sheet';
 import type { Controller } from '../../../../domain/protocols';
+import type { DataProps, OnFindEmailProps } from '../../../helper/find-email';
 import type { Request, Response } from 'express';
 
 interface Body {
   email: string;
   password: string;
   functionalityId: number;
+  googleSheets?: readGoogleSheetProps;
 }
 
 /**
- * @typedef {object} InsertStakeLoginCodeBody
+ * @typedef {object} InsertStakeAccountBannedBody
  * @property {string} email.required
  * @property {string} password.required
  * @property {number} functionalityId.required
+ * @property {GoogleSheets} googleSheets
  */
 
 /**
@@ -37,8 +33,8 @@ interface Body {
  * @security BearerAuth
  * @example request - payload example
  * {
- *   "email": "ElizabethAndrews828770@outlook.com",
- *   "password": "Elizabeth7242"
+ *   "email": "MicheleSaunders369520@outlook.com",
+ *   "password": "Michele9150"
  * }
  * @param {InsertStakeLoginCodeBody} request.body.required
  * @return {DefaultResponse} 200 - Successful response - application/json
@@ -47,141 +43,94 @@ interface Body {
 export const stakeLoginCodeController: Controller =
   () => async (request: Request, response: Response) => {
     try {
-      await insertEmailSchema.validate(request, { abortEarly: false });
+      const { email, password, googleSheets, functionalityId } = request.body as Body;
 
-      const { email, password, functionalityId } = request.body as Body;
+      const finalResults: DataProps[] = [];
 
-      const emails: string[] = [];
+      const convertResult = (result: string): string => {
+        switch (result) {
+          case 'LOGIN failed.':
+            return 'Erro ao fazer login';
 
-      const imapConfig = {
-        connTimeout: 999999999,
-        host: 'outlook.office365.com',
-        password,
-        port: 993,
-        tls: true,
-        user: email
+          default:
+            return result ?? 'Error';
+        }
       };
 
-      const imap = new Imap(imapConfig);
+      const finishFunction = async (): Promise<void> => {
+        if (typeof functionalityId === 'number') {
+          const data = finalResults.map((item) => ({
+            data: item.data,
+            functionalityId,
+            result: item.result,
+            userId: request.user.id
+          }));
 
-      let lastMatch;
-
-      const searchEmail = (): void => {
-        imap.search(
-          [
-            ['FROM', 'noreply@stake.com'],
-            ['SUBJECT', 'Entrar em Stake.com']
-          ],
-          (err2, results) => {
-            if (err2) throw new Error('');
-            if (results.length > 0) {
-              lastMatch = results[results.length - 1];
-
-              const fetch = imap.fetch([lastMatch], { bodies: '' });
-
-              fetch.on('message', (msg) => {
-                msg.on('body', (stream) => {
-                  let buffer = '';
-
-                  stream.on('data', (chunk) => {
-                    buffer += chunk.toString('utf8');
-                  });
-
-                  stream.on('end', () => {
-                    const regex = /<td>(?:\d{6})<\/td>/gu;
-                    const match = buffer.match(regex);
-
-                    if (match) emails.push(match?.[0].replace('<td>', '').replace('</td>', ''));
-                  });
-                });
-              });
-
-              fetch.once('end', () => {
-                imap.closeBox(() => {
-                  searchNextMailbox();
-                });
-              });
-            } else searchNextMailbox();
-          }
-        );
-      };
-
-      const mailboxesToSearch = ['JUNK', 'INBOX'];
-
-      const searchNextMailbox = (): void => {
-        if (mailboxesToSearch.length > 0) {
-          const nextMailbox = mailboxesToSearch.shift();
-
-          imap.openBox(nextMailbox!, true, (err, box) => {
-            if (err) throw new Error('');
-
-            if (box.messages.total > 0) searchEmail();
-            else searchNextMailbox();
+          await DataSource.action.createMany({
+            data,
+            skipDuplicates: true
           });
-        } else imap.end();
-      };
-
-      imap.once('ready', () => {
-        const initialMailbox = mailboxesToSearch.shift();
-
-        imap.openBox(initialMailbox!, true, (err, box) => {
-          if (err) throw new Error('');
-
-          if (box.messages.total > 0) searchEmail();
-          else searchNextMailbox();
-        });
-      });
-
-      imap.once('error', () => {
-        imap.end();
-      });
-
-      imap.once('end', async () => {
-        if (emails.length > 0) {
-          if (functionalityId)
-            await DataSource.action.create({
-              data: {
-                data: {
-                  email,
-                  senha: password
-                },
-                functionalityId,
-                hasError: false,
-                result: emails,
-                userId: request.user.id
-              }
-            });
-
-          return ok({ payload: emails, response });
         }
 
-        if (functionalityId)
-          await DataSource.action.create({
-            data: {
-              data: {
-                email,
-                senha: password
-              },
-              functionalityId,
-              hasError: true,
-              result: ['E-mail não encontrado'],
-              userId: request.user.id
-            }
-          });
-        return notFound({
-          entity: {
-            english: 'Email',
-            portuguese: 'E-mail'
+        ok({ payload: finalResults, response });
+        response.end();
+      };
+
+      const onFindEmail = ({ result, buffer }: OnFindEmailProps): void => {
+        const regex = /<td>(?:\d{6})<\/td>/gu;
+        const match = buffer.match(regex);
+
+        if (match !== null) result.push(match?.[0].replace('<td>', '').replace('</td>', ''));
+      };
+
+      const onEnd = (data: DataProps): void => {
+        let newResult: DataProps | undefined;
+
+        if (data.hasError && typeof data.errorMessage === 'string') {
+          newResult = { ...data, result: [convertResult(data.errorMessage)] };
+          data.result.push(convertResult(data.errorMessage));
+        } else if (data.result.length === 0) {
+          newResult = { ...data, result: ['Código não encontrado'] };
+          data.result.push('Código não encontrado');
+        } else newResult = data;
+
+        finalResults.push(newResult);
+      };
+
+      const functionToExec = async (data: functionToExecProps): Promise<string[]> => {
+        const res = await findEmail({
+          email: data.email,
+          from: 'noreply@stake.com',
+          onEnd,
+          onFindEmail,
+          password: data.password,
+          subject: 'Entrar em Stake.com'
+        });
+
+        return res;
+      };
+
+      if (typeof googleSheets === 'undefined') {
+        await functionToExec({ email, password });
+        await finishFunction();
+      } else if (Number(googleSheets.endRow) >= Number(googleSheets.startRow)) {
+        await readGoogleSheet({
+          ...googleSheets,
+          functionToExec
+        });
+        await finishFunction();
+      } else {
+        badRequest({
+          message: {
+            english: 'Final de linha tem que ser maior que início',
+            portuguese: 'Final de linha tem que ser maior que início'
           },
           response
         });
-      });
-
-      imap.connect();
+        response.end();
+      }
     } catch (error) {
       errorLogger(error);
-
-      if (error instanceof ValidationError) return validationErrorResponse({ error, response });
 
       return messageErrorResponse({ error, response });
     }
