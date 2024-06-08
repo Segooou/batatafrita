b7bcce6fd2b340d89e4999578f7acc3c4700dbba
iti <italo.felipe.ignacio@hotmail.com>
2024-06-08 12:35:09 -0300

    fdsf

--- src/main/utils/get-one-locale/index.ts
+++ src/main/utils/get-one-locale/index.ts
@@ -1,34 +1,32 @@
 export const getOneLocale = (): string => {
-  const states = [
-    'Acre, AC',
-    'Alagoas, AL',
-    'Amapá, AP',
-    'Amazonas, AM',
-    'Bahia, BA',
-    'Ceará, CE',
-    'Distrito Federal, DF',
-    'Espírito Santo, ES',
-    'Goiás, GO',
-    'Maranhão, MA',
-    'Mato Grosso, MT',
-    'Mato Grosso do Sul, MS',
-    'Minas Gerais, MG',
-    'Pará, PA',
-    'Paraíba, PB',
-    'Paraná, PR',
-    'Pernambuco, PE',
-    'Piauí, PI',
-    'Rio de Janeiro, RJ',
-    'Rio Grande do Norte, RN',
-    'Rio Grande do Sul, RS',
-    'Rondônia, RO',
-    'Roraima, RR',
-    'Santa Catarina, SC',
-    'São Paulo, SP',
-    'Sergipe, SE',
-    'Tocantins, TO'
+  const cities = [
+    'São Paulo/SP',
+    'Guarulhos/SP',
+    'Campinas/SP',
+    'São Bernardo do Campo/SP',
+    'Osasco/SP',
+    'Santo André/SP',
+    'São José dos Campos/SP',
+    'Ribeirão Preto/SP',
+    'Sorocaba/SP',
+    'Mauá/SP',
+    'São José do Rio Preto/SP',
+    'Mogi das Cruzes/SP',
+    'Santos/SP',
+    'Diadema/SP',
+    'Jundiaí/SP',
+    'Piracicaba/SP',
+    'Carapicuíba/SP',
+    'Bauru/SP',
+    'Itaquaquecetuba/SP',
+    'São Vicente/SP',
+    'Franca/SP',
+    'Praia Grande/SP',
+    'Guarujá/SP',
+    'Taubaté/SP',
+    'Limeira/SP'
   ];
-  const randomIndex = Math.floor(Math.random() * states.length);
+  const randomIndex = Math.floor(Math.random() * cities.length);
 
-  return states[randomIndex];
+  return cities[randomIndex];
 };
